9e3d10f6fbdb29232974900b8e40f6e551496606
JontySudha <102976767+JontySudha@users.noreply.github.com>
2025-09-05 23:37:53 +0530

    Update apiPaths.js

--- frontend/expense-tracker/src/utils/apiPaths.js
+++ frontend/expense-tracker/src/utils/apiPaths.js
@@ -1,6 +1,7 @@
-export const BASE_URL = "http://localhost:8000";
-
 // utils/apiPaths.js
+export const BASE_URL =
+  import.meta.env.VITE_API_URL || "http://localhost:8000";
+
 export const API_PATHS = {
   AUTH: {
     LOGIN: "/api/v1/auth/login",
@@ -10,19 +11,20 @@ export const API_PATHS = {
   DASHBOARD: {
     GET_DATA: "/api/v1/dashboard",
   },
-  INCOME:{
+  INCOME: {
     ADD_INCOME: "/api/v1/income/add",
     GET_ALL_INCOME: "/api/v1/income/get",
-    DELETE_INCOME: (incomeId) =>  `/api/v1/income/${incomeId}`,
+    DELETE_INCOME: (incomeId) => `/api/v1/income/${incomeId}`,
     DOWNLOAD_INCOME: `/api/v1/income/downloadexcel`,
   },
-  EXPENSE:{
+  EXPENSE: {
     ADD_EXPENSE: "/api/v1/expense/add",
     GET_ALL_EXPENSE: "/api/v1/expense/get",
-    DELETE_EXPENSE: (expenseId) =>  `/api/v1/expense/${expenseId}`,
+    DELETE_EXPENSE: (expenseId) => `/api/v1/expense/${expenseId}`,
     DOWNLOAD_EXPENSE: `/api/v1/expense/downloadexcel`,
   },
   IMAGE: {
     UPLOAD_IMAGE: "/api/v1/auth/upload-image",
   },
 };
+
