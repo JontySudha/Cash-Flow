da920af3857c18d794257f056faa12b670ee49ff
JontySudha <102976767+JontySudha@users.noreply.github.com>
2025-09-06 00:22:34 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,11 +2,15 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {});
-    console.log("MongoDB connected");
-  } catch (err) {
-    console.error("Error connecting to MongoDB", err);
-    process.exit(1);
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error("❌ MongoDB connection error:", error.message);
+    process.exit(1); // stop server if DB fails
   }
 };
 
